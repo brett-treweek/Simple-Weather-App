e07684081b16719e0a0f6a801f9dfb8a4d3f77ca
Brett Treweek <bretttrew@gmail.com>
2021-04-10 17:55:33 +0800

    js for city current parameters working and moment.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 const key = '7ab7e1141f7f2e8aa639df16840788da';
-const temp = document.querySelector('#temp');
 const searchButton = document.querySelector('#searchBtn');
+const cityHeading = document.querySelector('#city');
+const cityDate = document.querySelector('#date');
+const wind = document.querySelector('#wind');
+const uv = document.querySelector('#uv');
+const humidity = document.querySelector('#humidity');
+const icon = document.querySelector('#icon');
+const temp = document.querySelector('#temp');
+// const m = moment();
+
+
 
 
 searchButton.addEventListener('click', function(){
@@ -12,23 +21,60 @@ const query = `?q=${city}&units=metric&appid=${key}`;
 
 fetch(base + query)
   .then(function (response) {
-      console.log(response)
-    return response.json();
-  })
+    return response.json();})
   .then(function (data) {
-    console.log(data)
-    console.log(data.main.temp);
-    temp.innerText =' ' +data.main.temp;
+    cityHeading.innerText = data.name + ' ';
+    const cityLat = data.coord.lat;
+    const cityLon = data.coord.lon;
+    const oneBase = 'https://api.openweathermap.org/data/2.5/onecall';
+    const oneQuery = `?lat=${cityLat}&lon=${cityLon}&exclude=minutely,hourly&units=metric&appid=${key}`;
+        fetch(oneBase + oneQuery)
+        .then(function (response) {
+            return response.json();})
+        .then(function (data) {
+            console.log(data);
+            var date = (data.current.dt);
+            var timestamp = moment.unix(date);
+            var degree = data.current.wind_dir;
+            temp.innerText =' ' +data.current.temp;
+            cityDate.innerText = timestamp.format('dddd Do MMMM');
+            wind.innerText = data.current.wind_speed + 'm/s';
+            uv.innerText = data.current.uvi;
+            humidity.innerText = data.current.humidity + '%';
+            
+            
   });
 });
+});
+
+// console.log(timestamp.format('dddd Do MMMM'));
+//             function  windDirection(degree){
+//                 if (degree>337.5) return 'Northerly';
+//                 if (degree>292.5) return 'North Westerly';
+//                 if (degree>247.5) return 'Westerly';
+//                 if (degree>202.5) return 'South Westerly';
+//                 if (degree>157.5) return 'Southerly';
+//                 if (degree>122.5) return 'South Easterly';
+//                 if (degree>67.5) return 'Easterly';
+//                 if (degree>22.5) return 'North Easterly';
+//                 return 'Northerly';
+//             }
+
+// console.log( timestamp.format("HH/mm/ss") );
+
+
+
+// daily[0].dt
 
 
 
 
+// console.log(data.main.temp);
 
 
-
-
+// icon fetch------------------------------------
+// icon.innerText = data.weather[0].icon;
+// http://openweathermap.org/img/wn/10d@2x.png
 
 
 
